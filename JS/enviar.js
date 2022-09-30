349b0ecffc06f01282920755671ddd05a80ea1b0
Alba <alba.alonso@ikasle.egibide.org>
2022-09-30 21:08:20 +0200

    solucionar problemas merge

--- JS/enviar.js
+++ JS/enviar.js
@@ -6,13 +6,16 @@ document.getElementById("boton-label").addEventListener('change', function() {
         console.log("Checkbox is checked..");
         document.getElementById("manual").style.visibility = "hidden";
         document.getElementById("automatico").style.visibility = "visible";
+        document.getElementById("estados").style.visibility = "visible";
     } else {
         console.log("Checkbox is not checked..");
         document.getElementById("manual").style.visibility = "visible";  
         document.getElementById("automatico").style.visibility = "hidden";
+        document.getElementById("estados").style.visibility = "hidden";
     }
 });
 
 //Inicializar texto botones
 document.getElementById("manual").style.visibility = "hidden";
 document.getElementById("automatico").style.visibility = "visible";
+document.getElementById("estados").style.visibility = "visible";
