0b2e1e2f5ee5a26a2ae532c52dfe99d0f4bbb6c6
imanol <imanol.urquijo@ikasle.egibide.org>
2022-10-04 13:51:44 +0200

    Lista de paradas

---
+++ JS/desplegable.js
@@ -0,0 +1,31 @@
+        linea1();
+        linea2();
+
+
+        function linea1() {
+            var select = document.getElementById("linea1"); 
+            var abetxuko_florida=["Abetxuko","Kristo","Kañabenta","Artapadura","Arriaga","Arbol de Gernika",
+                        "Portal de Foronda","Intermodal","Honduras","Europa","Sancho el Sabio",
+                        "Lovaina","Parlamento","Angulema","Florida"];
+            for(var i = 0; i < abetxuko_florida.length; i++) { 
+                var opt = abetxuko_florida[i]; 
+                var el = document.createElement("option"); 
+                el.textContent = opt; 
+                el.value = opt; 
+                select.appendChild(el); 
+            }
+        }
+
+        function linea2() {
+            var select = document.getElementById("linea2"); 
+            var ibaiondo_universidad=["Ibaiondo","Landaverde","Lakuabizkarra","Wellington","Txagorritxu","Euskal Herria",
+                            "Honduras","Europa","Sancho el Sabio","Lovaina","Parlamento","Angulema","Florida",
+                            "Hegoalde","Universidad"];
+            for(var i = 0; i < ibaiondo_universidad.length; i++) { 
+                var opt = ibaiondo_universidad[i]; 
+                var el = document.createElement("option"); 
+                el.textContent = opt; 
+                el.value = opt; 
+                select.appendChild(el); 
+            }
+        }
