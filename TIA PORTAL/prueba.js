b24f2c4b588d39cc04cd3c783858c5f0b3af0060
Alba <alba.alonso@ikasle.egibide.org>
2022-10-05 13:30:53 +0200

    recibir datos
     de forma no asyncrona

--- TIA PORTAL/prueba.js
+++ TIA PORTAL/prueba.js
@@ -4,41 +4,64 @@ var paro_js
 var emergencia_js
 var receivedData;
 
+/*
+function receiveData () {
+
+    //Espera (await) una respuesta de fetch()
+	//Sabemos que el método then() será el que se disparará si todo sale bien.
+	try {
+		receivedData = await fetch("API.html").then((response) => response.json()).then((data) =>{return data});
+	} catch(e) {
+		alert(e);
+	}
+	marchaF();
+	paroF();
+	console.log(marcha_js);
+	console.log(paro_js);
+ } 
+
+async function recharge() {
+	while(true) {
+		let timelapse = new Promise(function(resolve) {
+			setTimeout(receiveData(), 3000);
+		 });
+		await timelapse;
+		console.log("regarga")
+	}
+}
+
+recharge();
+*/  
+
+receiveData();
+
 //Primero necesitamos crear una función asíncrona
 
 async function receiveData () {
 
     //Espera (await) una respuesta de fetch()
-     receivedData = await fetch ("API.html")
-    //Sabemos que el método then() será el que se disparará si todo sale bien.
-        .then((response) => response.json()).then((data) =>{
-            return data
-        })
-        .catch(err => {
-            alert('promise rejected: ', err);
-        });
-    } 
+	//Sabemos que el método then() será el que se disparará si todo sale bien.
+	try {
+		receivedData = await fetch("API.html").then((response) => response.json()).then((data) =>{return data});
+	} catch(e) {
+		alert(e);
+	}
+	marchaF();
+	paroF();
+	console.log(marcha_js);
+	console.log(paro_js);
+  } 
 
-    function marchaF(){
-       marcha_js = receivedData ['marcha'];
-    }
+ 
+function marchaF(){
+	marcha_js = receivedData ['marcha'];
+ }
 
-    function paroF(){
-        paro_js = receivedData ['paro'];
-     }
+function paroF(){
+     paro_js = receivedData ['paro'];
+}
     
-    function emergenciaF(){
-        emergencia_js = receivedData ['emergencia'];
-     }
-     
-    
-    
-      
-
-    
-
-
-
-
-
-
+function emergenciaF(){
+    emergencia_js = receivedData ['emergencia'];
+}
+           
