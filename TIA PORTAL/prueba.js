a4a86a1e6931cce40afa1e3e66b4fcb7baa2fe59
Ania <aniasura.khiari@ikasle.egibide.org>
2022-10-05 09:19:40 +0200

    correción del JS y json para la lectura de variables en el tia portal

--- TIA PORTAL/prueba.js
+++ TIA PORTAL/prueba.js
@@ -1,35 +1,43 @@
 
 var marcha_js;
-var paro_js;
-var emergencia_js;
+var paro_js
+var emergencia_js
+var receivedData;
+
+//Primero necesitamos crear una función asíncrona
+
+async function receiveData () {
+
+    //Espera (await) una respuesta de fetch()
+     receivedData = await fetch ("API.html")
+    //Sabemos que el método then() será el que se disparará si todo sale bien.
+        .then((response) => response.json()).then((data) =>{
+            return data
+        })
+        .catch(err => {
+            alert('promise rejected: ', err);
+        });
+    } 
+
+    function marchaF(){
+       marcha_js = receivedData ['marcha'];
+    }
+
+    function paroF(){
+        paro_js = receivedData ['paro'];
+     }
+    
+    function emergenciaF(){
+        emergencia_js = receivedData ['emergencia'];
+     }
+     
+    
+    
+      
+
+    
 
 
-
-function recogidaDatos(){
-
-	fetch ("API.html")
-	.then((response) => {
-
-		//Comprobamos que el estado de la respuesta sea valido
-
-		if(response.ok)
-
-			return response.text();
-
-		throw Error ("Error en la peticion");	
-
-		})
-
-		.then ((data) => {
-			alert(data);
-		});
-		.catch ((err)=> {
-			alert(err);
-
-		});
-
-	}
-	window.onload = enviarMarcha();
 
 
 
