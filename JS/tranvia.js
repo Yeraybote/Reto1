f686b101e555a1e3881cf6f9cfaea52dc454f9be
Yeraybote <yeray.bote@ikasle.egibide.org>
2022-10-05 10:36:08 +0200

    Botones simulación en funcionamiento

--- JS/tranvia.js
+++ JS/tranvia.js
@@ -1,4 +1,7 @@
+// Variables globales
 var parada = 0;
+var quieto = false;
+var emergencia = false;
 
 let moverp1 = function() {
   
@@ -72,4 +75,74 @@ let moverhome = function() {
 
    // Especificamos en que parada está
    parada = 0;
+}
+
+let automatico = function () {
+  quieto = false;
+  setTimeout(homep1,1000);
+  setTimeout(p1p2,7000);
+  setTimeout(p2p3,13000);
+  setTimeout(p3p2,19000);
+  setTimeout(p2p1,25000);
+  setTimeout(p1home,31000);
+}
+
+/* Movimientos para la función de automatico */
+function homep1() {
+  if (quieto == false) {
+  document.getElementById("train").style.animation = 'home-p1 5s'
+  document.getElementById("train").style.animationFillMode = 'forwards';
+  }
+  else parada = 0;
+}
+
+function p1p2() {
+  if (quieto == false) {
+  document.getElementById("train").style.animation = 'p1-p2 5s'
+  document.getElementById("train").style.animationFillMode = 'forwards';
+  }
+  else parada = 1;
+}
+
+function p2p3() {
+  if (quieto == false) {
+  document.getElementById("train").style.animation = 'p2-p3 5s'
+  document.getElementById("train").style.animationFillMode = 'forwards';
+  }
+  else parada = 2;
+}
+
+function p3p2() {
+  if (quieto == false) {
+  document.getElementById("train").style.animation = 'p3-p2 5s'
+  document.getElementById("train").style.animationFillMode = 'forwards';
+  }
+  else parada = 3;
+}
+
+function p2p1() {
+  if (quieto == false) {
+  document.getElementById("train").style.animation = 'p2-p1 5s'
+  document.getElementById("train").style.animationFillMode = 'forwards';
+  }
+  else parada = 2;
+}
+
+function p1home() {
+  if (quieto == false) {
+  document.getElementById("train").style.animation = 'p1-home 5s'
+  document.getElementById("train").style.animationFillMode = 'forwards';
+  }
+  else parada = 1;
+}
+
+/************************* Funciones paro y emergencia **************************/
+
+let paro = function (){
+  quieto = true;
+}
+
+let emer = function (){
+  emergencia = true;
+  moverhome();
 }
