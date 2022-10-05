c4caf2f37dc38a3dee5d305548e9148f932064cc
Alba <alba.alonso@ikasle.egibide.org>
2022-10-05 19:31:56 +0200

    alinear automático/manual al botón

--- JS/enviar.js
+++ JS/enviar.js
@@ -6,12 +6,12 @@ var boton_label = document.getElementById("boton-label");
 if(boton_label) {
     boton_label.addEventListener('change', function() {
         if (this.checked) {
-            console.log("Checkbox is checked..");
+            //console.log("Checkbox is checked..");
             document.getElementById("manual").style.visibility = "hidden";
             document.getElementById("automatico").style.visibility = "visible";
             document.getElementById("estados").style.visibility = "visible";
         } else {
-            console.log("Checkbox is not checked..");
+            //console.log("Checkbox is not checked..");
             document.getElementById("manual").style.visibility = "visible";  
             document.getElementById("automatico").style.visibility = "hidden";
             document.getElementById("estados").style.visibility = "hidden";
