53b1479dd629f32d75f29e20eafdd8800abcc743
Alba <alba.alonso@ikasle.egibide.org>
2022-10-06 13:19:43 +0200

    Logro actualizacion envio
    Trabajado en LiveSesion con Ania

--- TIA PORTAL/prueba.js
+++ TIA PORTAL/prueba.js
@@ -3,6 +3,7 @@ var marcha_js;
 var paro_js
 var emergencia_js
 var receivedData;
+var sendData;
 
 /*
 function receiveData () {
@@ -33,10 +34,13 @@ async function recharge() {
 recharge();
 */  
 
-receiveData();
+document.getElementById("marcha").addEventListener("click", sendDataMarcha);
+document.getElementById("paro").addEventListener("click", sendDataParo);
+document.getElementById("emergencia").addEventListener("click", sendDataEmergencia);
+
+// receiveData();
 
 //Primero necesitamos crear una función asíncrona
-
 async function receiveData () {
 
     //Espera (await) una respuesta de fetch()
@@ -52,12 +56,87 @@ async function receiveData () {
 	console.log(paro_js);
 	
 	console.log(receivedData);
+	sendData = receiveData;
+} 
+
+ 
+
+  let interval;
+	
+  async function recharge() {
+	 if(!interval){
+		interval = setInterval (receiveData,1000);
+	 }
+	
+}
+recharge();
+
+async function sendDataMarcha () {
+
+    //Espera (await) una respuesta de fetch()
+	//Sabemos que el método then() será el que se disparará si todo sale bien.
+	try {
+		sendData = new URLSearchParams (new FormData(document.getElementById("marcha")));
+		console.log(sendData);
+		var test = fetch('API.html', {
+			method: 'POST',
+			body: sendData
+		})		
+		.then(response => response.json())
+		.then((data) =>{return data});
+	
+	} catch(e) {
+		alert(e);
+	}
+	receiveData();
+	console.log(test);
   } 
 
+async function sendDataParo () {
+
+    //Espera (await) una respuesta de fetch()
+	//Sabemos que el método then() será el que se disparará si todo sale bien.
+	try {
+		sendData = new URLSearchParams (new FormData(document.getElementById("paro")));
+		console.log(sendData);
+		var test = fetch('API.html', {
+			method: 'POST',
+			body: sendData
+		})		
+		.then(response => response.json())
+		.then((data) =>{return data});
+	
+	} catch(e) {
+		alert(e);
+	}
+	receiveData();
+	console.log(test);
+} 
+
+async function sendDataEmergencia () {
+
+    //Espera (await) una respuesta de fetch()
+	//Sabemos que el método then() será el que se disparará si todo sale bien.
+	try {
+		sendData = new URLSearchParams (new FormData(document.getElementById("emergencia")));
+		console.log(sendData);
+		var test = fetch('API.html', {
+			method: 'POST',
+			body: sendData
+		})		
+		.then(response => response.json())
+		.then((data) =>{return data});
+	
+	} catch(e) {
+		alert(e);
+	}
+	receiveData();
+	console.log(test);
+} 
  
 function marchaF(){
 	marcha_js = receivedData ['marcha'];
- }
+}
 
 function paroF(){
      paro_js = receivedData ['paro'];
@@ -66,4 +145,3 @@ function paroF(){
 function emergenciaF(){
     emergencia_js = receivedData ['emergencia'];
 }
-           
