2e0a16d80cae2d2a2532e57c42e80f7d42b32390
Alba <alba.alonso@ikasle.egibide.org>
2022-10-06 13:28:51 +0200

    dejar autorecarga activada

--- TIA PORTAL/prueba.js
+++ TIA PORTAL/prueba.js
@@ -5,40 +5,9 @@ var emergencia_js
 var receivedData;
 var sendData;
 
-/*
-function receiveData () {
-
-    //Espera (await) una respuesta de fetch()
-	//Sabemos que el método then() será el que se disparará si todo sale bien.
-	try {
-		receivedData = await fetch("API.html").then((response) => response.json()).then((data) =>{return data});
-	} catch(e) {
-		alert(e);
-	}
-	marchaF();
-	paroF();
-	console.log(marcha_js);
-	console.log(paro_js);
- } 
-
-async function recharge() {
-	while(true) {
-		let timelapse = new Promise(function(resolve) {
-			setTimeout(receiveData(), 3000);
-		 });
-		await timelapse;
-		console.log("regarga")
-	}
-}
-
-recharge();
-*/  
-
 document.getElementById("marcha").addEventListener("click", sendDataMarcha);
 document.getElementById("paro").addEventListener("click", sendDataParo);
 document.getElementById("emergencia").addEventListener("click", sendDataEmergencia);
-
-// receiveData();
 
 //Primero necesitamos crear una función asíncrona
 async function receiveData () {
@@ -54,21 +23,21 @@ async function receiveData () {
 	paroF();
 	console.log(marcha_js);
 	console.log(paro_js);
-	
 	console.log(receivedData);
 	sendData = receiveData;
 } 
 
- 
-
-  let interval;
+let interval;
 	
-  async function recharge() {
+async function recharge() {
 	 if(!interval){
 		interval = setInterval (receiveData,1000);
 	 }
 	
 }
+
+//receiveData();
+
 recharge();
 
 async function sendDataMarcha () {
