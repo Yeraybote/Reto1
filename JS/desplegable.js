80b8fc9959906a1eb65b7e05bf8da1958e58f476
imanol <imanol.urquijo@ikasle.egibide.org>
2022-10-07 13:02:17 +0200

    desplegable final

--- JS/desplegable.js
+++ JS/desplegable.js
@@ -1,3 +1,4 @@
+
 /**
 * @author Grupo 1, 142GA DAW <wat2022.wordpress.com>
 * 
@@ -5,7 +6,7 @@
 
 
 
-//EVENTOS el primero afecta a toda la pagina y el segundo a un boton.
+        //EVENTOS el primero afecta a toda la pagina y el segundo a un boton.
         document.addEventListener('DOMContentLoaded', flinea1); 
         document.getElementById("consultar").addEventListener("click",fdestino);
 
@@ -92,7 +93,7 @@
 
 
 
-        //Esta funcion busca la posicion de la parada seleccionada en el array
+        //Esta funcion busca la posicion de la parada seleccionada, en el array.
        function parada1(a,b) {
 
             var e = document.getElementById("origen");
@@ -108,6 +109,9 @@
             }
         };
 
+
+        //Es lo mismo que la anterior funcion pero antes de comprobar si hay coincidencias
+        //le damos la vuelta al array que contiene las paradas.
         function parada2(a,b) {
 
             var e = document.getElementById("origen");
@@ -125,25 +129,32 @@
         };
 
 
+
+        //Esta funcion calcula los horarios del tranbia
         function horario(a,b){
 
 
+            //creamos una fecha culaquiera con 0 horas y 15 minutos, nos quedamos con los minutos.
             let date1=new Date(2022,0,1,0,15);
             var mins=date1.getMinutes();
 
+            //Creamos una fecha con el tiempo actual y nos quedamos con las horas.
             let actual=new Date();
             let dtext2=actual.getHours();
 
+            //Añadimos la hora actual a la hora de comienzo del tranvia.
             let dtext1=b[a].setHours(dtext2);
             dtext1=b[a].toTimeString();
             dtext1=dtext1.split(' ')[0];
 
+            //Visualizamos la primera hora.
             let visualizar=document.getElementById("hora");
             visualizar.innerHTML=dtext1;
 
             var select=document.getElementById("reloj");
 
-
+            //Dado que el tranbia pasa cada 15 minutos, a la primera hora le sumamos 15 minutos 3 veces.
+            //La nueva hora se la pasamos a un nuevo elemento h1 que creamos con cada repeticion.
             let date2=b[a];
             for (var i = 0; i < 3; i++) {
                 date2.setMinutes(date2.getMinutes()+mins);
